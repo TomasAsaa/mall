d69f8e115a03029cf9e51d87f3be13ff6926f883
atstudy <1192475589@qq.com>
2023-03-16 19:19:13 +0800

    商品图片完成

---
+++ src/components/spu.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<img :src="'http://localhost:8001/img/' + selected_img" style="width: 200px;">
+		<div class="mt-2">
+			<img v-for="img of data.attrKeyList[0].attrValueList" :key="img.value_id + img.value_name"
+				:src="'http://localhost:8001/img/' + img.value_images[0]" style="width: 30px;"
+				:class="{'border-red' : selected_img == img.value_images[0]}"
+				@click="img_click(img.value_images[0])"
+				:title="img.value_images[0]">
+								
+		</div>
+		<div class="text-bolder text-lg  text-red mt-2">1999</div>
+		<div class="text-sm text-secondary mt-2">{{data.spu_name}}</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['data'],
+		data() {
+			return{
+				selected_img : this.data.attrKeyList[0].attrValueList[0].value_images[0]
+			}
+		},
+		
+		mounted() {
+			console.log(this.selected_img)
+		},
+		
+		methods : {
+			img_click(x){
+				this.selected_img = x
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- src/components/spulist.vue
+++ src/components/spulist.vue
@@ -1,28 +1,18 @@
 <template>
 	<div class="d-flex flex-wrap">
-		<div v-for="spu of list" :key="spu.spu_id + spu.spu_name" style="width: 200px;" class="mx-3 mt-6">
-			<img :src="'http://localhost:8001/img/' + spu.attrKeyList[0].attrValueList[0].value_images
-[0]" style="width: 200px;">
-			<div class="mt-2">
-				<img v-for="img of spu.attrKeyList[0].attrValueList" :key="img.value_id + img.value_name"
-				:src="'http://localhost:8001/img/' + img.value_images[0]"				
-				style="width: 30px;">				
-			</div>
-			<div class="text-bolder text-lg  text-red mt-2">1999</div>
-			<div class="text-sm text-secondary mt-2">{{spu.spu_name}}</div>
-		</div>		
+		<spu v-for="spu of list" :key="spu.spu_id + spu.spu_name" style="width: 200px;" class="mx-3 mt-6" :data ='spu'></spu>
 	</div>
 </template>
 
 <script>
-	// import spu from '@/components/spu.vue'
+	import spu from '@/components/spu.vue'
 
 	export default {
 		props: ['list'],
 
-		// components : {
-		// 	spu
-		// }
+		components : {
+			spu
+		}
 	}
 </script>
 
